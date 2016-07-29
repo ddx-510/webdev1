b1499e0d8e924ccf3ec225b269e1937cd2776b05
ddx <dai.tianle@dhs.sg>
2016-07-29 15:45:59 +0800

    test

---
+++ app.js
@@ -0,0 +1,16 @@
+$(function() {
+// code for clicking button .etc
+
+    $("#getJoke").click(
+        function() {
+            $.get("/jokes",function(data){
+                // TODO:
+                // modify this code to manipulate the HTML
+                // to display the setup and punchline on the page
+                // instead of using alerts!
+                $('#setup').html(data.setup);
+				$('#punchline').html(data.punchline);
+            },"json")
+        }
+    );
+});
